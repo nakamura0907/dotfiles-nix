9ca4bc09fda67b3d29191004c1c76d36cf871e62
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-03-23 18:07:18 +0900

    init commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Home Manager configuration of nakamura0907";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells = {
+          default = import ./shells/shell.nix { inherit pkgs; };
+        };
+
+        homeConfigurations."nakamura0907" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [ ./home.nix ];
+
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+      }
+  );
+}
