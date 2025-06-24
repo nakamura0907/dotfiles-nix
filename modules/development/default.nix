89c50d0e448866f70e0ec9d13171e290786caa16
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-06-25 00:00:46 +0900

    rust-srcの追加

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -8,7 +8,10 @@
 		# node
 		nodejs_22 pnpm_10
 		# rust
-		rust-bin.stable.latest.default # oxalica/rust-overlay
+		(rust-bin.stable.latest.default.override {
+			extensions = [ "rust-src" ];
+   			targets = ["aarch64-unknown-linux-gnu"];
+ 		})  # oxalica/rust-overlay
 
 		ansible
 		docker colima
