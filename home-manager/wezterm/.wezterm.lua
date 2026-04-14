ba91433141c06a0cc5735ded4a4a064a4826e6aa
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2026-04-15 00:03:08 +0900

    weztermを透過するよう設定

--- home-manager/wezterm/.wezterm.lua
+++ home-manager/wezterm/.wezterm.lua
@@ -1,18 +1,13 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
+config.automatically_reload_config = true 
 
--- This is where you actually apply your config choices.
-
--- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
-
--- or, changing the font size and color scheme.
-config.font_size = 10
 config.color_scheme = 'AdventureTime'
+config.font_size = 14
+config.use_ime = true
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
 
--- Finally, return the configuration to wezterm:
+config.window_decorations = "RESIZE"
+
 return config
